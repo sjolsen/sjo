aef5bd93c238828193209d290b6856ccf52cf43f
Stuart Olsen <so1132@txstate.edu>
2013-12-30 19:25:30 -0600

    Added lifting

---
+++ lift.hh
@@ -0,0 +1,24 @@
+#ifndef SJO_LIFT_HH
+#define SJO_LIFT_HH
+
+#include <utility>
+
+#define LIFT(member) \
+[] (auto&& obj) -> decltype (auto) \
+{ \
+	return std::forward <decltype (obj)> (obj).member; \
+}
+
+#define MFLIFT(member) \
+[] (auto&& obj, auto&&... args) -> decltype (auto) \
+{ \
+	return std::forward <decltype (obj)> (obj).member (std::forward <decltype (args)> (args)...); \
+}
+
+#define FLIFT(fname) \
+[] (auto&&... args) -> decltype (auto) \
+{ \
+	return fname (std::forward <decltype (args)> (args)...); \
+}
+
+#endif
